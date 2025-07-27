0b2315af7da030187b3d19a0743863e827a48be1
VIKAS S S <vikas.ss2023ece@sece.ac.in>
2025-07-27 11:18:56 +0530

    loginsignupupdated

--- HabitTracker/HabitTracker/src/main/java/com/example/HabitTracker/controller/AuthController.java
+++ HabitTracker/HabitTracker/src/main/java/com/example/HabitTracker/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class AuthController {
 
